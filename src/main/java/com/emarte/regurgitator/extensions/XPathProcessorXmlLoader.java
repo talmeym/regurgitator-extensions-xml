ce7b03aadc205268433e90f81a28f5203bcde832
talmeym <mtalmey@gmail.com>
2016-03-11 09:21:39 +0000

    tidied logs

--- src/main/java/com/emarte/regurgitator/extensions/XPathProcessorXmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/XPathProcessorXmlLoader.java
@@ -5,11 +5,12 @@ import org.dom4j.Element;
 
 import java.util.*;
 
+import static com.emarte.regurgitator.core.Log.getLog;
 import static com.emarte.regurgitator.extensions.ExtensionsConfigConstants.*;
 import static com.emarte.regurgitator.extensions.XmlNamespaceLoader.loadNamespaces;
 
 public class XpathProcessorXmlLoader implements XmlLoader<XpathProcessor> {
-	private static final Log log = Log.getLog(XpathProcessorXmlLoader.class);
+	private static final Log log = getLog(XpathProcessorXmlLoader.class);
 
 	@Override
 	public XpathProcessor load(Element element, Set<Object> allIds) throws RegurgitatorException {
