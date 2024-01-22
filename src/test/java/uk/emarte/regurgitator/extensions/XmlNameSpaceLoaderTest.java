581a96230fcd2989f65479e29a0b2e26b4cb0a6e
talmeym <mtalmey@gmail.com>
2024-01-22 12:18:28 +0000

    renamed tests

--- src/test/java/uk/emarte/regurgitator/extensions/XmlNameSpaceLoaderTest.java
+++ src/test/java/uk/emarte/regurgitator/extensions/XmlNameSpaceLoaderTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class XmlNameSpaceLoaderTest {
     @Test
-    public void testThis() throws Exception {
+    public void testNamespaceLoader() throws Exception {
         assertEquals("{rge=http://extensions.regurgitator.emarte.uk, rg=http://core.regurgitator.emarte.uk}", XmlNamespaceLoader.loadNamespaces(null, "rg=http://core.regurgitator.emarte.uk,rge=http://extensions.regurgitator.emarte.uk").toString());
     }
 }
